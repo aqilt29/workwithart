d46272c8bb7ae439895095a63f457a3996217ffc
aqilt29 <aqil.thanawala@gmail.com>
2020-10-11 22:46:11 -0700

    Init commit w connection to store api

---
+++ gatsby-config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-shopify`,
+      options: {
+        apiVersion: '2020-01',
+        // The domain name of your Shopify shop.
+        shopName: `wwastrore`,
+        // includeCollections: ['home', 'shop'],
+        verbose: true,
+        // The storefront access token
+        accessToken: `e687fe4b4c410b8b2d703e03955d0fef`,
+      },
+    },
+  ],
+}
