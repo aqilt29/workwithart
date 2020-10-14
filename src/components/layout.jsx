d5b26d0105ee436ce9f569af184a672a88fcc3c6
Aqil Thanawala <aqil.thanawala@gmail.com>
2020-10-14 08:50:27 -0700

    Stub out files for nav

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,53 +1,32 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
+import NavBar from './navbar';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import 'normalize.css'
 
-import Header from "./header"
-import "./layout.css"
+const PageBody = styled.div`
+  /* background-color: #2C3539;
+  height: auto; */
+`;
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    background-color: #2C3539;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <GlobalStyle />
+      <PageBody>
+        <NavBar />
+        <div>{children}</div>
+      </PageBody>
     </>
   )
-}
+};
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default Layout;
